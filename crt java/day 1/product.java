4c3021c3417de0cfa135c0438120064aeb336452
harika <maramshankar007@gmail.com>
2024-11-11 19:48:19 +0530

    Add files via upload

---
+++ crt java/day 1/product.java
@@ -0,0 +1,15 @@
+class product
+{
+public static void main(String args[])
+{
+int pno=101;
+String pname="soap";
+float pcost=135.99f;
+float pqty=95.56f;
+
+System.out.println("product number:"+pno);
+System.out.println("product name:"+pname);
+System.out.println("product cost:"+pcost);
+System.out.println("product qty:"+pqty);
+}
+}
