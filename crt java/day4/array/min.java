cf5b536384d59c7fe4eb8e10aa597fd43ca9af76
harika <maramshankar007@gmail.com>
2024-11-13 21:57:35 +0530

    Add files via upload

---
+++ crt java/day4/array/min.java
@@ -0,0 +1,8 @@
+import java.util.*;
+class min {
+public static void main(String[] args) {
+int a[]={1,423,6,46,34,23,13,53,4}; 
+ Arrays.sort(a);
+System.out.println("min"+a[0]+" max"+a[a.length-1]);
+}
+}
